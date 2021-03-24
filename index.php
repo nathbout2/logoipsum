b1bbeb607e58be2436b89ea540ed4caffac6c4d3
nathan <n.boutaud@codeur.online>
2021-03-24 16:55:22 +0100

    version0.01

--- index.php
+++ index.php
@@ -1,9 +1,47 @@
 <?php include 'header.php'; ?>
-
-
-
-
-
+<div class="container">
+<div class="container-fluid">
+        <img src ="image\logo.png">
+</div>
+<nav class="navbar navbar-expand-sm  justify-content-center align-items-start">
+    <!-- notice position-absolute on the brand which allows it to center-->
+    <div class="navbar-collapse-center" id="navbar">
+        <ul class="navbar-nav justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Blog</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+  <div class="row">
+  <div class="col-sm">
+      1
+    </div>
+    <div class="col-sm">
+      2
+    </div>
+    <div class="col-sm">
+      3
+    </div>
+    <div class="col-sm">
+     4
+    </div>
+    <div class="col-sm">
+      5
+    </div>
+  </div>
+</div>
+</div>
+</div>
 
 
 
