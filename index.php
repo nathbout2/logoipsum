2d4e0cc66cb11823d598a6371dd7412769b4791c
nathan <n.boutaud@codeur.online>
2021-03-25 11:56:49 +0100

    version0.2

--- index.php
+++ index.php
@@ -1,48 +1,119 @@
 <?php include 'header.php'; ?>
+<img src="image/logo.png" class="img-thumbnail" alt="...">
+<ul class="nav justify-content-center">
+<button type="button" class="btn btn-primary btn-lg">Get Started</button>
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Features</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Pricing</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Blog</a>
+  </li>
+</ul>
 <div class="container">
-<div class="container-fluid">
-        <img src ="image\logo.png">
-</div>
-<nav class="navbar navbar-expand-sm  justify-content-center align-items-start">
-    <!-- notice position-absolute on the brand which allows it to center-->
-    <div class="navbar-collapse-center" id="navbar">
-        <ul class="navbar-nav justify-content-center">
-            <li class="nav-item">
-                <a class="nav-link" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Blog</a>
-            </li>
-        </ul>
-    </div>
-</nav>
-  <div class="row">
-  <div class="col-sm">
-      1
+<div class="row">
+    <div class="col-sm">
+    <h2> Grow your subscription business</h2>
+    Outcome-centered products that reduce churn, optimize pricing, and grow your subscription business end-to-end.
+    <button type="button" class="btn btn-primary btn-lg">Get Started</button>
     </div>
     <div class="col-sm">
-      2
+    <img src="image/skater1.png">
     </div>
-    <div class="col-sm">
-      3
-    </div>
-    <div class="col-sm">
-     4
-    </div>
-    <div class="col-sm">
-      5
     </div>
   </div>
 </div>
+<div class="container">
+ <div class="row">
+  <img src="image/logo ligne.png" class="img-fluid" alt="...">
+ </div>
 </div>
+ <div class="container">
+  <div class="row align-items-start">
+    <div class="col">
+    <img src="image/intello.png">
+    </div>
+    <div class="col -sm 6">
+    <h3>Subscription index</h3>
+    <p><small>A daily dataset to keep you up to date on subscription market trends and what's happening in your vertical.
+    Learn morev
+    </div>
+    <div class="container">
+ <div class="row">
+ <div class="col-sm 6">
+ <h3>In-depth metrics</h3>
+ <p><small>Accurate, real-time reporting at your fingertips. Getting data has never been easier.
+    Learn more</small></p>
+    <img src="image/intello 2.png">
+    </div>
+    </div>
+  </div>
+  <div class="container">
+  <div class="row align-items-start">
+    <div class="col-sm 4">
+    <img src="image/carte.png" class="img-thumbnail" alt="...">
+    </div>
+    <p class="h5">Benchmarks</p>
+    <p><small></small>See how you stack up against comparable companies in similar stages.</p>
+    <div class="col-sm 4">
+    <img src="image/calculette.png" class="img-thumbnail" alt="...">
+    <p class="h5">Pricing Audit</p>
+    <p><small></small>Benchmark the health of your monetization and pricing strategy.</p>
+    </div>
+    <div class="col-sm 4 ">
+    <img src="image/camera.png" class="img-thumbnail" alt="...">
+    <p class="h5">Retention Audit</p>
+    <p><small></small>Audit where revenue leakage exists and where you can increase retention.</p>
+    </div>
+    </div>
+  </div>
+  <div class="container">
+  <div class="row align-items-start">
+    <div class="col">
+    <h3>Get the right plan for future product.</h3>
+    </div>
+    <div class="col">
+    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+  <button class="btn btn-primary me-md-2" type="button">Yearly</button>
+  <button class="btn btn-primary" type="button">Monthly</button>
 </div>
-
-
+    </div>
+    <div class="card-group">
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Starter</h5>
+      <p class="h4">Free</p>
+      <p class="card-text">1 Website
+5 GB Hosting
+Limited Suppor</p>
+      <button type="button" class="btn btn-primary btn-sm">Get Started</button>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Premium</h5>
+      <p class="h4">$29/month</p>
+      <p class="card-text">10 Website
+15 GB Hosting
+Premium Support</p>
+      <button type="button" class="btn btn-primary btn-sm">Get Started</button>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Enterprise</h5>
+      <p class="h4">$49/month</p>
+      <p class="card-text">Unlimited Website
+50 GB Hosting
+Premium Support</p>
+      <button type="button" class="btn btn-primary btn-sm">Get Started</button>
+    </div>
+  </div>
+</div>
 
 <?php include 'footer.php'; ?>
