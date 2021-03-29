4b2cba1d497ff3da56a0f16722883e1097043c04
nathan <n.boutaud@codeur.online>
2021-03-29 16:27:32 +0200

    version0.3.1

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php include 'header.php'; ?>
-<!--logo-->
-
-<!--fin logo-->
 
 <!--nav bar-->
 <ul class="nav justify-content-center">
@@ -18,88 +15,107 @@
      <li class="nav-item">
      <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Blog</a>
      </li>
-     <button type="button" class="btn1 btn-primary border-white btn-lg">Get Started</button>
+     <button type="button" class="btn1 btn-primary border-white btn-lg"><b>Get Started</b></button>
 </ul>
 <!--fin nav bar-->
 
 <!--page acc-->
-<div class="container-fluid">
+<div class="container">
      <div class="row">
-         <div class="col-sm">
-         <h2> Grow your subscription business</h2>
-         Outcome-centered products that reduce churn, optimize pricing, and grow your subscription business end-to-end.
-         <button type="button" class="btn2 btn-primary border-white btn-lg">Get Started</button>
+         <div class="col-sm d-flex align-items-center justify-content-center">
+             <div class="row">
+                 <div class="col-12"><h1><b>Grow your subscription business</b></h1></div>
+                 <div class="col-12"><p class="lead"> Outcome-centered products that reduce churn, optimize pricing, and grow your subscription business end-to-end.</p></div>
+                 <div class="col-12"><h2><button type="button" class="btn2 btn-primary border-white btn-lg"><b>Get Started</b></button></h2></div>
+             </div>
          </div>
          <div class="col-sm">
          <img src="image/skater-1.png">
          </div>
      </div>
 </div>
+</div>
 
 <!--fin page acc-->
 
 <!--logo-->
-<div class="container-fluid">
+<div class="container">
      <div class="row">
-     <img src="image/logo ligne.png" class="img-fluid" alt="...">
+        <div class="col-md-3 col-sm-4"><img src="image/logoipsum-1.svg" alt=""></div>
+        <div class="col-md-2 col-sm-4"><img src="image/logoipsum-2.svg" alt=""></div>
+        <div class="col-md-2 col-sm-4"><img src="image/logoipsum-3.svg" alt=""></div>
+        <div class="col-md-2 col-sm-4"><img src="image/logoipsum-4.svg" alt=""></div>
+        <div class="col-md-3 col-sm-4"><img src="image/logoipsum-5.svg" alt=""></div>
      </div>
+</div>
 </div>
 <!--fin logo-->
 
 <!--explication-->
- <div class="container-fluid">
+ <div class="container">
      <div class="row align-items-start">
-         <div class="col1">
+         <div class="col">
          <img src="image/intello-1.png">
          </div>
-         <div class="col">
-         <h3>Subscription index</h3>
-         <p><small>A daily dataset to keep you up to date on subscription market trends and what's happening in your vertical.
+         <div class="col-sm d-flex align-items-center justify-content-center text3">
+         <div class="row">
+                 <div class="col-12"><h1><b>Subscription index</b></h1></div>
+                 <div class="col-12"><p class="lead"> A daily dataset to keep you up to date on subscription market trends and what's happening in your vertical.</p></div>
+                 <div class="col-12"><p class="lead1"><b>Learn more</b></p></div>
          </div>
- <div class="container-fluid">
-     <div class="row">
-     <div class="col">
-     <h3>In-depth metrics</h3>
-     <p><small>Accurate, real-time reporting at your fingertips. Getting data has never been easier.
-     Learn more</small></p>
-     <img src="image/intello-2.png">
-     </div>
-     </div>
- </div>
+</div>
+</div>
+</div>
+ <div class="container">
+ <div class="row align-items-start">
+         <div class="col-sm d-flex align-items-center justify-content-center text3">
+         <div class="row">
+                 <div class="col-12"><h1><b>In-depth metrics</b></h1></div>
+                 <div class="col-12"><p class="lead">Accurate, real-time reporting at your fingertips. Getting data has never been easier.</p></div>
+                 <div class="col-12"><p class="lead1"><b>Learn more</b></p></div>
+         </div>
+         <div class="col">
+         <img src="image/intello-2.png">
+         </div>
+</div>
+</div>
+</div>
+
 <!-- fin explication-->
 
 <!--pub-->
-  <div class="container-fluid">
+<div class="container3">
      <div class="row align-items-start">
-          <div class="col-sm 4">
-             <img src="image/carte1.png" class="img-thumbnail" alt="...">
-         </div>
-             <p class="h5">Benchmarks</p>
+          <div class="col">
+             <img src="image/carte1.png" class="imgcarte" alt="...">
+             <p class="h5"><b>Benchmarks</b></p>
              <p><small></small>See how you stack up against comparable companies in similar stages.</p>
-          <div class="col-sm 4">
-             <img src="image/calculette1.png" class="img-thumbnail" alt="...">
-             <p class="h5">Pricing Audit</p>
+         </div>
+          <div class="col">
+             <img src="image/calculette1.png" class="imgcalculette" alt="...">
+             <p class="h5"><b>Pricing Audit</b></p>
              <p><small></small>Benchmark the health of your monetization and pricing strategy.</p>
          </div>
-          <div class="col-sm 4 ">
-             <img src="image/photo1.png" class="img-thumbnail" alt="...">
-             <p class="h5">Retention Audit</p>
+          <div class="col">
+             <img src="image/photo1.png" class="imgphoto" alt="...">
+             <p class="h5"><b>Retention Audit</b></p>
              <p><small></small>Audit where revenue leakage exists and where you can increase retention.</p>
          </div>
       </div>
  </div>
+</div>
 <!-- fin pub-->
 
 <!--choix-->
-  <div class="container-fluid">
+<div class="container4">
      <div class="row align-items-start">
          <div class="col">
-          <h3>Get the right plan for future product.</h3>
+          <h1><b>Get the right plan for future product.</b></h1>
          </div>
      <div class="col">
          <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-         <button class="btn btn-primary border-white" type="button">Yearly</button>
-         <button class="btn btn-primary border-white" type="button">Monthly</button>
+         <button class="btn btn-primary border-white" type="button"><b>Yearly</b></button>
+         <button class="btn btn-primary border-white" type="button"><b>Monthly</b></button>
          </div>
      </div>
      </div>
@@ -108,38 +124,39 @@
 
 
 <!-- carte mois-->
-<div class= container-fluid>
- <div class="card-group">
+<div class= container>
+ <div class="cardgroup1">
      <div class="card1">
          <div class="card-body">
-          <h5 class="card-title">Starter</h5>
-          <p class="h4">Free</p>
-          <p class="card-text">1 Website
-          5 GB Hosting
-          Limited Suppor</p>
-          <button type="button" class="btn5 btn-primary btn-sm">Get Started</button>
+          <h5 class="card-title1">Starter</h5>
+          <h2 class="h2">Free</h2>
+          <p class="card-text1">1 Website</p>
+          <p class="card-text1">5 GB Hosting</p>
+          <p class="card-text1">Limited Suppor</p>
+          <button type="button" class="btn5 btn-primary border-0 btn-sm"><b>Get Started</b></button>
          </div>
      </div>
      <div class="card2">
          <div class="card-body">
          <h5 class="card-title">Premium</h5>
-         <p class="h4">$29/month</p>
-         <p class="card-text">10 Website
-         15 GB Hosting
-         Premium Support</p>
-         <button type="button" class="btn6 btn-primary btn-sm">Get Started</button>
+         <h2 class="h2">$29/month</h2>
+         <p class="card-text">10 Website</p>
+         <p class="card-text">15 GB Hosting</p>
+         <p class="card-text">Premium Support</p>
+         <button type="button" class="btn6 btn-primary border-0 btn-sm"><b>Get Started</b></button>
          </div>
      </div>
      <div class="card3">
          <div class="card-body">
-         <h5 class="card-title">Enterprise</h5>
-         <p class="h4">$49/month</p>
-         <p class="card-text">Unlimited Website
-         50 GB Hosting
-         Premium Support</p>
-         <button type="button" class="btn7 btn-primary btn-sm">Get Started</button>
+         <h5 class="card-title1">Enterprise</h5>
+         <h2 class="h2">$49/month</h2>
+         <p class="card-text1">Unlimited Website</p>
+         <p class="card-text1">50 GB Hosting</p>
+         <p class="card-text1">Premium Suppors</p>
+         <button type="button" class="btn7 btn-primary border-0 btn-sm"><b>Get Started</b></button>
          </div>
      </div>
+</div>
 </div>
 </div>
 <!-- fin carte mois-->
